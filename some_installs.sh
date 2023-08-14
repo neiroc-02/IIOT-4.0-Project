02848330d78474f06cb26f67c4987160b6e6df16
neiro-c02 <neiro.cabrera@yahoo.com>
2023-08-14 10:30:20 -0700

    Revising script to install in /usr/include

--- some_installs.sh
+++ some_installs.sh
@@ -3,15 +3,17 @@
 #These are all the packages you must install for the project.
 #Most are for the GUI, mosquitto is what you use to set up the broker.
 
+#Installs necessary packages for linux systems (using Debian)
 packages=("mosquitto" "libglfw-dev" "libglfw3" "xorg-dev" "libxcb1-dev")
 for str in ${packages[@]}; 
 do
 	sudo apt install $str
 done
 
+#Clones necessary github repos in /usr/include
 githubs=("https://github.com/ocornut/imgui" "https://github.com/ShakaUVM/read")
 for str in ${githubs[@]}; 
 do
-	git clone $str
+	cd /usr/include/ && sudo git clone $str
 done
 
