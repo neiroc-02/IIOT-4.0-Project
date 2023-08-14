2c2662fb4c023c7f4469cd64c94e526d42b98d18
neiro-c02 <neiro.cabrera@yahoo.com>
2023-08-14 10:48:25 -0700

    Adjusting header file names to fit install script

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -21,7 +21,7 @@
 #include <mutex>
 #include <map>
 #include <cstring>
-#include "/home/neiropi/read/read.h"
+#include "read/read.h"
 #define GL_SILENCE_DEPRECATION
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <GLES2/gl2.h>
